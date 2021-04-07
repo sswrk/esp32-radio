078db51448b77270a4811601fe73155625c9f105
sswrk <swierk.szymon99@gmail.com>
2021-04-08 00:49:20 +0200

    started web server, added /stations POST endpoint

--- radio/radio.ino
+++ radio/radio.ino
@@ -1,6 +1,10 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <EEPROM.h>
+#include "ESPAsyncWebServer.h"
+#include "AsyncJson.h"
+#include "ArduinoJson.h"
+#include "SPIFFS.h"
 
 #define EEPROM_SIZE 2
 
@@ -8,6 +12,7 @@ long interval = 250;
 long previousButtonMillis = 0;
 
 int radioStation = 0;
+bool connectedToStation = false;
 int previousRadioStation = -1;
 const int previousButton = 13;
 const int nextButton = 15;
@@ -15,14 +20,44 @@ const int nextButton = 15;
 char ssid[] = "<SSID>";
 char pass[] = "<PASSWORD>";
 
-char *host[4] = {"centova.radios.pt", "wamu-1.streamguys.com", "realfm.live24.gr", "live.slovakradio.sk"};
-char *path[4] = {"/stream?type=http&nocache=3342", "/", "/realfm", "/Devin_256.mp3"};
-int   port[4] = {9496, 80, 80, 8000};
+String hosts[100] = {"centova.radios.pt", "wamu-1.streamguys.com", "realfm.live24.gr", "live.slovakradio.sk"};
+String paths[100] = {"/stream?type=http&nocache=3342", "/", "/realfm", "/Devin_256.mp3"};
+int   ports[100] = {9496, 80, 80, 8000};
+
+int availableStations = 3;
+
+AsyncWebServer server(80);
 
 int status = WL_IDLE_STATUS;
 WiFiClient  client;
 uint8_t mp3buff[32];
 
+//server methods
+void notFound(AsyncWebServerRequest *request){
+  request->send(404, "application/json", "{\"message\":\"Not found\"}");
+}
+
+bool addRadioStation(const char* host, const char* path, int port){
+  if(host==NULL || path==NULL || port==NULL){
+    return false;
+  }
+  hosts[availableStations] = String(host);
+  paths[availableStations] = String(path);
+  ports[availableStations] = port;
+  availableStations++;
+  
+  Serial.println("Added radio station! Available radio stations: ");
+  for(int i=0; i<availableStations; i++){
+    Serial.print(hosts[i]);
+    Serial.print(":");
+    Serial.print(ports[i]);
+    Serial.print(paths[i]);
+    Serial.println("");
+  }
+  return true;
+}
+
+//buttons methods
 void IRAM_ATTR previousButtonInterrupt() {
 
   unsigned long currentMillis = millis();
@@ -33,7 +68,7 @@ void IRAM_ATTR previousButtonInterrupt() {
     if (radioStation > 0)
       radioStation--;
     else
-      radioStation = 3;
+      radioStation = availableStations - 1;
 
     previousButtonMillis = currentMillis;
   }
@@ -46,7 +81,7 @@ void IRAM_ATTR nextButtonInterrupt() {
 
     Serial.println("NEXT");
 
-    if (radioStation < 3)
+    if (radioStation < availableStations - 1)
       radioStation++;
     else
       radioStation = 0;
@@ -68,10 +103,43 @@ void setup () {
   attachInterrupt(digitalPinToInterrupt(previousButton), previousButtonInterrupt, RISING);
   attachInterrupt(digitalPinToInterrupt(nextButton), nextButtonInterrupt, RISING);
 
+  if(!SPIFFS.begin(true)){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
   connectToWIFI();
 
+  //server requests handling
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.on("/stations", HTTP_POST, [](AsyncWebServerRequest *request){
+    }, NULL, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total){
+      DynamicJsonDocument doc(1024);
+      DeserializationError error = deserializeJson(doc, (const char*)data);
+      bool success = false;
+      if(!error) {
+        const char* host = doc["host"];
+        const char* path = doc["path"];
+        int port = doc["port"];
+        success = addRadioStation(host, path, port);
+      }
+      if(success){
+        request->send(200, "text/plain", "Radio station added");
+      } else {
+        request->send(400, "text/plain", "Wrong parameters");
+      }
+  });
+
+  server.onNotFound(notFound);
+
+  server.begin();
+
+  //set radio station
   radioStation = getLastStationFromEEPROM();
-  if (radioStation < 0 || radioStation >= 4) {
+  if (radioStation < 0 || radioStation >= availableStations) {
     radioStation = 0;
   }
   previousRadioStation = radioStation;
@@ -79,7 +147,7 @@ void setup () {
 
 void loop() {
 
-  if (radioStation != previousRadioStation) {
+  if (radioStation != previousRadioStation || (connectedToStation==false && availableStations>0)) {
     connectToStation(radioStation);
     previousRadioStation = radioStation;
     writeLastStationToEEPROM(radioStation);
@@ -97,13 +165,16 @@ void loop() {
 
 void connectToStation(int stationId ) {
   Serial.println("Connecting to radio station...");
-  if (client.connect(host[stationId], port[stationId]) ) {
+  char buf[100];
+  hosts[stationId].toCharArray(buf, 100);
+  if (client.connect(buf, ports[stationId])) {
     Serial.println("Connected to radio station!");
   }
-  client.print(String("GET ") + path[stationId] + " HTTP/1.1\r\n" +
-               "Host: " + host[stationId] + "\r\n" +
+  client.print(String("GET ") + paths[stationId] + " HTTP/1.1\r\n" +
+               "Host: " + hosts[stationId] + "\r\n" +
                "Connection: close\r\n\r\n");
   drawRadioStationName(stationId);
+  connectedToStation = true;
 }
 
 void connectToWIFI() {
@@ -113,10 +184,12 @@ void connectToWIFI() {
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void drawRadioStationName(int id) {
-  Serial.println(host[id]);
+  Serial.println(hosts[id]);
 }
 
 int getLastStationFromEEPROM() {
