ccaa21074758a3d0e5e83204de07994db0d883ac
Andrii Trishch <andrii.t@webchefs.pl>
2021-05-05 23:25:24 +0200

    added boostrap

--- radio/data/script.js
+++ radio/data/script.js
@@ -1,32 +1,85 @@
 let currentStation = '302.1';
+let favouritesStationsArr = [];
+let savedStationsArr = [];
+let isPlaying = false;
 
-var xmlhttp = new XMLHttpRequest();
+let xmlhttp = new XMLHttpRequest();
+
+function fetchStations() {
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            savedStationsArr = JSON.parse(this.responseText);
+        }
+    }
+    xmlhttp.open("GET", "/stations", true);
+    xmlhttp.send()
+}
 
 function updateCurrentStation(newStation) {
     currentStation = newStation;
     document.getElementById('current-station').innerHTML = currentStation;
 }
 
-function executeLoop() {
-    updateCurrentStation(currentStation);
+
+function addStationToFavorites(id) {
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            this.savedStationsArr.find((s) => s.id === id).isFavourite = true;
+        }
+    }
+    xmlhttp.open("POST", "/stations", true);
+    xmlhttp.send('id=' + id);
 }
 
-async function setPrevStation() {
+async function selectPreviousStation(id) {
     xmlhttp.onreadystatechange = function () {
-        if (this.readyState = 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             updateCurrentStation(this.responseText)
         }
     }
-    xmlhttp.open("GET", "/station/prev", true);
-    xmlhttp.send()
+    xmlhttp.open("POST", "/station/set-station", true);
+    xmlhttp.send('id=' + id)
 }
 
 function togglePlayState() {
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            isPlaying = !isPlaying;
+        }
+    }
 
+    xmlhttp.open("GET", "/control/toggle-play", true);
+    xmlhttp.send();
 }
 
-function setNextStation() {
+function selectNextStation() {
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            updateCurrentStation(this.responseText)
+        }
+    }
+    xmlhttp.open("POST", "/station/set-station", true);
+    xmlhttp.send('id=' + id)
+}
 
+function makeMoreQuiet() {
+    xmlhttp.onreadystatechange = () => {
+    };
+    xmlhttp.open("GET", "/control/more-quiet", true)
+    xmlhttp.send();
+}
+
+function makeLouder() {
+    xmlhttp.onreadystatechange = () => {
+    };
+    xmlhttp.open("GET", "/control/louder", true)
+    xmlhttp.send();
+}
+}
+
+function executeLoop() {
+    fetchStations();
+    updateCurrentStation(currentStation);
 }
 
 executeLoop()
